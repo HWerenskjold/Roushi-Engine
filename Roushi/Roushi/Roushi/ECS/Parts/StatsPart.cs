55af41b081d394299bd7d18badf9ad53a1b38a21
Zombiedance <Rilliden@gmail.com>
2016-12-13 09:18:08 +0900

    Initial Upload

---
+++ Roushi/Roushi/Roushi/ECS/Parts/StatsPart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roushi
+{
+    class StatsPart : Part
+    {
+        private float health;
+        private float maxHealth;
+        private float stamina;
+
+        public StatsPart(float health, float stamina)
+        {
+            this.health = health;
+            this.maxHealth = health;
+            this.stamina = stamina;
+        }
+
+        public float getHealth()
+        { return health; }
+
+        public float getMaxHealth()
+        { return maxHealth; }
+
+        public void setHealth(float hp)
+        {
+            if (hp > maxHealth)
+                this.health = maxHealth;
+            else
+                this.health = hp;
+        }
+
+        public float getStamina()
+        { return stamina; }
+    }
+}
